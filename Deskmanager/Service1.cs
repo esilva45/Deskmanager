83dde143197081a8b56c57827af2fb8deab155db
Elso Silva <esilva45@hotmail.com>
2020-03-11 10:00:51 -0300

    Adicionar arquivos de projeto.

---
+++ Deskmanager/Service1.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceProcess;
+using System.Threading;
+
+namespace ServiceIntegration {
+    public partial class Service : ServiceBase {
+        private Timer _timer;
+
+        public Service() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+            _timer = new Timer(ProcessorManager, null, 0, 60000);
+        }
+
+        protected override void OnStop() { }
+
+        private void ProcessorManager(object state) {
+            SendMessage.Message();
+        }
+    }
+}
